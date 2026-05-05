02a700ee55619262d1fe43478b0595423165fe40
kthomas127 <34731501+kthomas127@users.noreply.github.com>
2026-05-05 00:17:31 -0400

    add preliminary styling to the UI

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
 const container = document.getElementById('container');
+const GRID_SIZE_PX = 320;
 
 createGrid();
 function createGrid(size=16){
+    const squareSize = GRID_SIZE_PX / size;
     container.innerHTML = '';
-    container.style.width = (size * 20) + 'px';
+    container.style.width = GRID_SIZE_PX + 'px';
+    container.style.height = GRID_SIZE_PX + 'px';
     for (let i = 0; i < size; i++){
         for (let j = 0; j< size; j++){
             const square = document.createElement('div');
             square.classList.add('grid-square');
+            square.style.width = squareSize + 'px';
+            square.style.height = squareSize + 'px';
             container.append(square);
         }
     }
